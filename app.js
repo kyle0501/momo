60abab5a29b24e76155c6b13a1763cd0ac77bc84
kyle0501 <kyle0501@users.noreply.github.com>
2019-01-13 14:02:03 +0800

    123

---
+++ app.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var firebase = require("firebase");
+var app = express();
+const puppeteer = require('puppeteer');
+var config = {
+    apiKey: "AIzaSyCBzq2c88vfanIWRBw73hLfhPkJ7aTDJmI",
+    authDomain: "h-599f1.firebaseapp.com",
+    databaseURL: "https://h-599f1.firebaseio.com",
+    projectId: "h-599f1",
+    storageBucket: "h-599f1.appspot.com",
+    messagingSenderId: "723873673565"
+  };
+  firebase.initializeApp(config);
+  firebase.initializeApp(config);
+var db = firebase.database();
+app.get('/', function (req, res) {
+
+
+let scrape = async () => {
+     const browser = await puppeteer.launch({headless: false});
+    // const browser = await puppeteer.launch({headless:true, args: ['--no-sandbox', '--disable-setuid-sandbox']}); 
+    const page = await browser.newPage();
+    
+    await page.goto('https://www.momoshop.com.tw/search/searchShop.jsp?keyword=ps4%20%E4%B8%BB%E6%A9%9F%20pro&searchType=1&curPage=1&_isFuzzy=0&showType=chessboardType');
+    
+    const result = await page.evaluate(() => {
+        let title_data = [];
+        let price_data = [];
+        let titles  = document.querySelectorAll('.searchPrdListArea .listArea ul li .prdName'); 
+            
+            for (var element of titles){ 
+                let title = element.innerText; 
+                title_data.push({title}); 
+        }
+        let prices = document.querySelectorAll(".searchPrdListArea .listArea ul li .money .price");
+
+            for (var element of prices) {
+                let price = element.innerText;
+                price = price.replace('$', '');
+                price_data.push({ price });
+            }
+        return {title_data,price_data}; 
+    });
+
+    var products = new Array();
+    for (r in result.title_data) {
+    products.push({ title: result.title_data[r].title, price: result.price_data[r].price, website: "Momo" });
+        }
+      return products;
+  }
+  scrape().then((value) => {
+    var ref = db.ref("/");
+    ref.set(value);
+    ref.once("value", function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            var key = childSnapshot.key;
+            var childData = childSnapshot.val();              
+          
+            var title_val = childSnapshot.val().title;
+            var price_val = childSnapshot.val().price;
+            var website_val = childSnapshot.val().website;
+            $("#title").append(title_val);
+            $("#price").append(price_val);
+            $("#website").append(website_val);
+            });
+          });
+    console.log(snapshot.val());
+});
+    // res.send(value);
+ });
+
+var server = app.listen(8081, function () {
+})
